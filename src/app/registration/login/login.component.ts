7397d0834bd1dfe63d336e4ac40f84914a1bcac9
rsq-codequeen <159681694+rsq-codequeen@users.noreply.github.com>
2025-08-27 09:58:53 +0500

    learnt observable

--- src/app/registration/login/login.component.ts
+++ src/app/registration/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    data:any[]=[];
+    datas:any[]=[];
+    observable=new Observable((observer)=>{
+      observer.next([1,2,3,4,5])
+    })
+    secondObservable=new Observable((observer)=>{
+     setTimeout(()=>{observer.next(1)},1000 )
+      setTimeout(()=>{observer.next(1)},2000 )
+      setTimeout(()=>{observer.next(1)},3000 )
+      setTimeout(()=>{observer.next(1)},4000 )
+      setTimeout(()=>{observer.next(8)},5000 )
+    })
+    GetSyncData(){
+      console.log('yess')
+      this.observable.subscribe((val:any)=>{
+        this.data=val;
+      })
+    }
 
+    GetSecondObservable(){
+      this.secondObservable.subscribe((val:any)=>{
+        this.datas.push(val)
+      })
+    }
 }
